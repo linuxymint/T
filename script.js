8990adb6305a2b44ed1350dd37a78f22de174d66
linuxymint <linuxmint1978@gmail.com>
2025-01-14 23:43:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,65 +19,14 @@ themeToggle.addEventListener('click', () => {
 
 // قائمة القنوات
 const channels = [
-    
-    {
-        name: 'Channel 1',
-        url: 'http://a7-60.5-194.cdn13.com/30000/30944/30944.flv',
-        logo: 'https://via.placeholder.com/80' // يمكنك استبدالها برابط شعار القناة
-    },
-    {
-        name: 'Channel 2',
-        url: 'http://a7-60.5-194.cdn13.com/30000/30938/30938.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 3',
-        url: 'http://a7-60.5-194.cdn13.com/30000/30920/30920.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 4',
-        url: 'http://a7-60.5-77.cdn13.com/30000/30933/30933.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 5',
-        url: 'http://a7-60.5-77.cdn13.com/30000/30919/30919.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 6',
-        url: 'http://a7-60.5-194.cdn13.com/30000/30932/30932.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 7',
-        url: 'http://a7-60.5-201.cdn13.com/30000/30939/30939.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 8',
-        url: 'http://a7-60.5-201.cdn13.com/30000/30918/30918.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 9',
-        url: 'http://a7-60.5-194.cdn13.com/30000/30930/30930.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
-        name: 'Channel 10',
-        url: 'http://a7-60.5-69.cdn13.com/28000/28974/28974.flv',
-        logo: 'https://via.placeholder.com/80'
-    },
-    {
+        {
         name: 'Channel 11',
         url: 'http://a7-60.5-194.cdn13.com/28000/28978/28978.flv',
         logo: 'https://via.placeholder.com/80'
     },
     {
         name: 'Channel 12',
-        url: 'http://a7-60.5-77.cdn13.com/25000/25543/25543.flv',
+        url: 'https://adultm3u.com/dl/x-adultm3u-com-20-12-2021-4.m3u',
         logo: 'https://via.placeholder.com/80'
     },
     {
