d0f0ead52c365c925a9c43c94d4df77bb5950dfc
linuxymint <linuxmint1978@gmail.com>
2025-01-14 23:39:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,74 +19,7 @@ themeToggle.addEventListener('click', () => {
 
 // قائمة القنوات
 const channels = [
-    // القنوات العراقية
-    {
-        name: 'Al-Hurra Iraq',
-        url: 'https://mbnvvideoingest-i.akamaihd.net/hls/live/1004674/MBNV_ALHURRA_IRAQ/playlist.m3u8',
-        logo: 'https://i.imgur.com/mXBZEQP.png'
-    },
-    {
-        name: 'Al-Hurra',
-        url: 'https://mbnvvideoingest-i.akamaihd.net/hls/live/1004673/MBNV_ALHURRA_MAIN/playlist.m3u8',
-        logo: 'https://i.imgur.com/0izeu5z.png'
-    },
-    {
-        name: 'Al-Iraqiya',
-        url: 'https://cdn.catiacast.video/abr/8d2ffb0aba244e8d9101a9488a7daa05/playlist.m3u8',
-        logo: 'https://i.imgur.com/imdV6kL.png'
-    },
-    {
-        name: 'Al-Rafidain',
-        url: 'https://cdg8.edge.technocdn.com/arrafidaintv/abr_live/playlist.m3u8',
-        logo: 'https://i.imgur.com/D78qG91.png'
-    },
-    {
-        name: 'Al-Rasheed',
-        url: 'https://media1.livaat.com/AL-RASHEED-HD/tracks-v1a1/playlist.m3u8',
-        logo: 'https://i.imgur.com/SU9HbXY.png'
-    },
-    {
-        name: 'Al-Sharqiya News',
-        url: 'https://5d94523502c2d.streamlock.net/alsharqiyalive/mystream/playlist.m3u8',
-        logo: 'https://i.imgur.com/P6p17ZY.jpg'
-    },
-    {
-        name: 'Al-Sharqiya',
-        url: 'https://5d94523502c2d.streamlock.net/home/mystream/playlist.m3u8',
-        logo: 'https://i.imgur.com/bPYyXNf.png'
-    },
-    {
-        name: 'Dijlah Tarab',
-        url: 'https://ghaasiflu.online/tarab/tracks-v1a1/playlist.m3u8',
-        logo: 'https://i.imgur.com/2SBjjBQ.png'
-    },
-    {
-        name: 'Dijlah TV',
-        url: 'https://ghaasiflu.online/Dijlah/tracks-v1a1/playlist.m3u8',
-        logo: 'https://i.imgur.com/FJEeYiz.png'
-    },
-    {
-        name: 'iNEWS',
-        url: 'https://svs.itworkscdn.net/inewsiqlive/inewsiq.smil/playlist.m3u8',
-        logo: 'https://i.imgur.com/PeuBkaH.png'
-    },
-    {
-        name: 'Iraq Future Ⓢ',
-        url: 'https://streaming.viewmedia.tv/viewsatstream40/viewsatstream40.smil/playlist.m3u8',
-        logo: 'https://i.imgur.com/Z7woTe5.png'
-    },
-    {
-        name: 'Turkmeneli TV',
-        url: 'https://137840.global.ssl.fastly.net/edge/live_6b7c6e205afb11ebb010f5a331abaf98/playlist.m3u8',
-        logo: 'https://i.imgur.com/iUhhg4B.png'
-    },
-    {
-        name: 'Zagros TV',
-        url: 'https://5a3ed7a72ed4b.streamlock.net/zagrostv/SMIL:myStream.smil/playlist.m3u8',
-        logo: 'https://i.imgur.com/UjIuIQX.png'
-    },
-
-    // القنوات الجديدة
+    
     {
         name: 'Channel 1',
         url: 'http://a7-60.5-194.cdn13.com/30000/30944/30944.flv',
